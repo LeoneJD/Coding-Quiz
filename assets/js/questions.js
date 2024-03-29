338e651a3e735803368fe8c8fab913863c0c09ef
Leone Dale <dalejleone@gmail.com>
2024-03-29 23:45:19 +0000

    added styling

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+// create an array of of objects for questions
+
+const questions = [
+  {
+    question: "Which of the following is a JavaScript data type?",
+    choices: ["String", "HTML", "CSS", "File"],
+    answer: "String"
+  },
+  {
+    question: "How do you declare a JavaScript variable?",
+    choices: ["var carName;", "v carName;", "variable carName;", "var = carName;"],
+    answer: "var carName;"
+  },
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onchange", "onclick", "onmouseover", "onmouseclick"],
+    answer: "onclick"
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    choices: ["function:myFunction()", "function=myFunction()", "function myFunction()", "create myFunction()"],
+    answer: "function myFunction()"
+  },
+  {
+    question: "How do you call a function named 'myFunction'?",
+    choices: ["call function myFunction()", "call myFunction()", "myFunction()", "execute myFunction"],
+    answer: "myFunction()"
+  },
+  {
+    question: "How to write an IF statement in JavaScript?",
+    choices: ["if i == 5 then", "if i = 5", "if (i == 5)", "if i = 5 then"],
+    answer: "if (i == 5)"
+  },
+  {
+    question: "How does a FOR loop start?",
+    choices: ["for (i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", "for (i = 0; i <= 5; i++)"],
+    answer: "for (i = 0; i <= 5; i++)"
+  },
+  {
+    question: "How can you add a comment in a JavaScript?",
+    choices: ["'This is a comment", "//This is a comment", "<!--This is a comment-->", "(This is a comment)"],
+    answer: "//This is a comment"
+  },
+  {
+    question: "What is the correct way to write a JavaScript array?",
+    choices: ["var colors = 'red', 'green', 'blue'", "var colors = ['red', 'green', 'blue']", "var colors = (1:'red', 2:'green', 3:'blue')", "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')"],
+    answer: "var colors = ['red', 'green', 'blue']"
+  },
+  {
+    question: "Which operator is used to assign a value to a variable?",
+    choices: ["X", "-", "*", "="],
+    answer: "="
+  }
+];
